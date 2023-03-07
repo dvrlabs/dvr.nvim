8212891d1fd0f54b670ec582adeb5ca937c48572
dvr <dvrlabs.tv@gmail.com>
2023-03-07 12:12:03 -0500

    Added diagnostics, delete buffer -> close buffer

--- init.lua
+++ init.lua
@@ -96,10 +96,11 @@ vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = tr
 vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
 
 -- Diagnostic keymaps
-vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = "Go to previous diagnostic message" })
-vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = "Go to next diagnostic message" })
--- vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = "Open floating diagnostic message" })
--- vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = "Open diagnostics list" })
+WK.register({ d = { name = "Diagnostics", } }, { prefix = "<leader>" })
+vim.keymap.set('n', '<leader>dp', vim.diagnostic.goto_prev, { desc = "Go to previous diagnostic message" })
+vim.keymap.set('n', '<leader>dn', vim.diagnostic.goto_next, { desc = "Go to next diagnostic message" })
+vim.keymap.set('n', '<leader>df', vim.diagnostic.open_float, { desc = "Open floating diagnostic message" })
+vim.keymap.set('n', '<leader>dl', vim.diagnostic.setloclist, { desc = "Open diagnostics list" })
 
 -- Ui 'Toggle' keymaps
 WK.register({ u = { name = "UI", } }, { prefix = "<leader>" })
@@ -137,7 +138,7 @@ vim.keymap.set('n', '<leader>v', '<C-w>v', { noremap = true, silent = true, desc
 
 -- Delete buffer
 -- WK.register({ b = { name = "Buffer", } }, { prefix = "<leader>" })
-vim.keymap.set('n', '<leader>d', ':bd!<CR>', { desc = "Delete Buffer", noremap = true, silent = true })
+vim.keymap.set('n', '<leader>c', ':bd!<CR>', { desc = "Close Buffer", noremap = true, silent = true })
 
 -- Quit Window
 vim.keymap.set('n', '<leader>q', ':q<CR>', { desc = "Quit" })
